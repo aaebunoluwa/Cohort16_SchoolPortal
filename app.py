fa62c14c6c614c27d8e45c5d9260e070f4f3eba0
Gbeke Iyun <gbekeiyun@gmail.com>
2025-05-05 12:01:22 +0100

    Your message here

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ def home():
 
 @app.route('/about')
 def about():
-    return "This is not the About Page."
+    return "Hello, This is not the About Page."
 
 if __name__ == '__main__':
     app.run(debug=True)
